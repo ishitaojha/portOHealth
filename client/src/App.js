2b9b6789ae50fc7b7e2919c729118b93bf71d8e3
prakhar728 <prakharojha12@gmail.com>
2021-10-30 11:23:40 +0530

    Initial commit

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import About from "./components/About/About";
+import Header from "./components/Header/Header";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <About />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/Header/Header.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import './Header.css';
+function Header() {
+    return (
+        <>
+            <nav>
+                <ul>
+                    <li>
+                        <a href="#Home">Home</a>
+                    </li>
+                    <li>
+                    <a href="#About">About HIV/AIDs</a>
+                    </li>
+                    <li>
+                    <a href="#chat">Consult with us!</a>
+                    </li>
+                </ul>
+            </nav>
+        </>
+    )
+}
+
+export default Header
